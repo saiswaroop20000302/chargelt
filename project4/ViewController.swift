1fdfad5d5f3ac66cc7dace20fca858aa25ec98c1
swaroop <swaroopss020@gmail.com>
2026-01-16 15:14:20 -0600

    Initial Commit

---
+++ project4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project4
+//
+//  Created by Veera Venkata Sai Swaroop Grandhi on 1/16/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ project4UITests/project4UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  project4UITestsLaunchTests.swift
+//  project4UITests
+//
+//  Created by Veera Venkata Sai Swaroop Grandhi on 1/16/26.
+//
+
+import XCTest
+
+final class project4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
